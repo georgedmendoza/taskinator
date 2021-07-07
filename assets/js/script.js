f795827be6a3f70b2aac32288fe2c9dada27c796
Your Name <you@example.com>
2021-07-07 14:37:59 -0500

    save tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,6 +64,8 @@ var createTaskEl = function(taskDataObj) {
 
     //increase task counter for next unique id
     taskIdCounter++;
+
+    saveTasks();
 }
 
 var createTaskActions = function(taskId) {
@@ -125,6 +127,8 @@ var deleteTask = function(taskId) {
     }
     //reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+
+    saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -182,6 +186,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
     //reseting from and changin button text back to normal
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
+
+    saveTasks();
 }
 
 var taskStatusChangeHandler = function(event) {
@@ -209,8 +215,13 @@ var taskStatusChangeHandler = function(event) {
             tasks[i].status = statusValue;
         }
     }
-    console.log(tasks);
+
+    saveTasks();
 };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
 
 pageContentEl.addEventListener("click",taskButtonHandler);
 pageContentEl.addEventListener("change",taskStatusChangeHandler);
